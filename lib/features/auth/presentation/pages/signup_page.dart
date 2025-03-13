69d26e2529db8c97e63a2a22019c1928323c3b99
Yash Chandra <yash24.chandra2003@gmail.com>
2025-03-13 23:57:40 +0530

    Sign Up Page Design Done

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,4 +1,6 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -13,7 +15,7 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(15),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -26,10 +28,28 @@ class _SignupPageState extends State<SignupPage> {
             ),
             SizedBox(height: 30),
             AuthField(hintText: 'Name'),
-            SizedBox(height: 10,),
+            SizedBox(height: 15),
             AuthField(hintText: 'Email'),
-            SizedBox(height: 10,),
+            SizedBox(height: 15),
             AuthField(hintText: 'Password'),
+            SizedBox(height: 20),
+            AuthGradientButton(),
+            SizedBox(height: 20),
+            RichText(
+              text: TextSpan(
+                text: 'Already Have an Account?  ',
+                style: Theme.of(context).textTheme.titleMedium,
+                children: [
+                  TextSpan(
+                    text: 'Sign In',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: AppPallete.gradient2,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
