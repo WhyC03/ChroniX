b8fed82668d45dff0a25ca96f8c6db133c9deb3c
Yash Chandra <yash24.chandra2003@gmail.com>
2025-03-28 00:03:17 +0530

    Login Function Done

--- lib/features/auth/data/data_sources/auth_remote_data_source.dart
+++ lib/features/auth/data/data_sources/auth_remote_data_source.dart
@@ -1,14 +1,15 @@
 import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/features/auth/data/models/user_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
-  Future<String> signUpWithEmailPassword({
+  Future<UserModel> signUpWithEmailPassword({
     required String name,
     required String email,
     required String password,
   });
 
-  Future<String> loginWithEmailPassword({
+  Future<UserModel> loginWithEmailPassword({
     required String email,
     required String password,
   });
@@ -19,15 +20,29 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
 
   AuthRemoteDataSourceImpl(this.supabaseClient);
   @override
-  Future<String> loginWithEmailPassword({
+  Future<UserModel> loginWithEmailPassword({
     required String email,
     required String password,
-  }) {
-    throw UnimplementedError();
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signInWithPassword(
+        password: password,
+        email: email,
+      );
+
+      if (response.user == null) {
+        throw ServerException('User is Null');
+      }
+      return UserModel.fromJson(
+        response.user!.toJson(),
+      );
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
   }
 
   @override
-  Future<String> signUpWithEmailPassword({
+  Future<UserModel> signUpWithEmailPassword({
     required String name,
     required String email,
     required String password,
@@ -44,7 +59,9 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
       if (response.user == null) {
         throw ServerException('User is Null');
       }
-      return response.user!.id;
+      return UserModel.fromJson(
+        response.user!.toJson(),
+      );
     } catch (e) {
       throw ServerException(e.toString());
     }
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,4 +1,6 @@
+import 'package:blog_app/core/common/widgets/loader.dart';
 import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/show_snackbar.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
@@ -44,75 +46,91 @@ class _SignupPageState extends State<SignupPage> {
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 110),
-                Text(
-                  'Sign Up.',
-                  style: TextStyle(
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 30),
-                AuthField(
-                  hintText: 'Name',
-                  controller: nameController,
-                ),
-                SizedBox(height: 15),
-                AuthField(
-                  hintText: 'Email',
-                  controller: emailController,
-                ),
-                SizedBox(height: 15),
-                AuthField(
-                  hintText: 'Password',
-                  controller: passwordController,
-                  isObscureText: true,
-                ),
-                SizedBox(height: 20),
-                AuthGradientButton(
-                  text: 'Sign Up',
-                  onTap: () {
-                    if (formKey.currentState!.validate()) {
-                      // log('Validated');
-                      context.read<AuthBloc>().add(
-                            AuthSignUp(
-                              email: emailController.text.trim(),
-                              password: passwordController.text.trim(),
-                              name: nameController.text.trim(),
-                            ),
-                          );
-                    }
-                  },
-                ),
-                SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, LoginPage.route());
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      text: 'Already have an account?  ',
-                      style: Theme.of(context).textTheme.titleMedium,
-                      children: [
-                        TextSpan(
-                          text: 'Sign In',
-                          style:
-                              Theme.of(context).textTheme.titleMedium?.copyWith(
+          child: BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is AuthFailure) {
+                showSnackBar(context, state.message);
+              }
+            },
+            builder: (context, state) {
+              if (state is AuthLoading) {
+                return Center(
+                  child: Loader(),
+                );
+              }
+              return Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 110),
+                    Text(
+                      'Sign Up.',
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    AuthField(
+                      hintText: 'Name',
+                      controller: nameController,
+                    ),
+                    SizedBox(height: 15),
+                    AuthField(
+                      hintText: 'Email',
+                      controller: emailController,
+                    ),
+                    SizedBox(height: 15),
+                    AuthField(
+                      hintText: 'Password',
+                      controller: passwordController,
+                      isObscureText: true,
+                    ),
+                    SizedBox(height: 20),
+                    AuthGradientButton(
+                      text: 'Sign Up',
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          // log('Validated');
+                          context.read<AuthBloc>().add(
+                                AuthSignUp(
+                                  email: emailController.text.trim(),
+                                  password: passwordController.text.trim(),
+                                  name: nameController.text.trim(),
+                                ),
+                              );
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, LoginPage.route());
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Already have an account?  ',
+                          style: Theme.of(context).textTheme.titleMedium,
+                          children: [
+                            TextSpan(
+                              text: 'Sign In',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium
+                                  ?.copyWith(
                                     color: AppPallete.gradient2,
                                     fontWeight: FontWeight.bold,
                                   ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ),
